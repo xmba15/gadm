2c62f5629eac19dc7fb23d1796102636957ccdbf
xmba15 <thba1590@gmail.com>
2023-10-14 15:21:54 +0900

    add timeout, fake browser headers

--- gadm/utils.py
+++ gadm/utils.py
@@ -2,15 +2,22 @@
 Supportive utility methods for gadm library
 """
 
+import logging
 import os
 
 import requests
 import tqdm
+from fake_useragent import UserAgent
 
 __all__ = ["download_url"]
 
 
-def download_url(url: str, file_path: str) -> None:
+def download_url(
+    url: str,
+    file_path: str,
+    timeout_sec: float = 60,
+    logger: logging.Logger = logging.getLogger(),
+) -> None:
     """download file from url
 
     Parameters
@@ -27,7 +34,12 @@ def download_url(url: str, file_path: str) -> None:
     if os.path.isfile(file_path) and os.path.getsize(file_path) == file_size:
         return
 
-    res = requests.get(url, stream=True)
+    try:
+        headers = {"User-Agent": str(UserAgent().random)}
+        res = requests.get(url, stream=True, timeout=timeout_sec, headers=headers)
+    except requests.exceptions.Timeout as e:
+        logger.exception(f"Failed to fetch data due to time out: {e}")
+        return
 
     with open(file_path, "wb") as _f:
         pbar = tqdm.tqdm(total=file_size, unit="B", unit_scale=True)
